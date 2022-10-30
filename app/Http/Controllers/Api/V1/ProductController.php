31c07b04a9d49c22afec043b72990ce405356134
kms69 <kavehmohammadi1369@gmail.com>
2022-10-30 10:24:56 +0330

    initial commit

---
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $id = $request->input(['id']);
+        if($id) {
+            $product = Product::findOrFail($id);
+
+            return response(['Product' => $product, 'message' => 'Successful'], 200);
+        }
+        $products = Product::all();
+
+        return response(['Product' => $products, 'message' => 'Successful'], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:products,name',
+        ]);
+
+        $input = $request->all();
+        $product = Product::create($input);
+
+        return response(['Product' => $product, 'message' => 'Successful'], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return response(['Product' => $product, 'message' => 'Successful'], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => ['required', Rule::unique('products', 'name')
+                ->ignore($id)],
+        ]);
+
+        $input = $request->all();
+        $product = Product::findOrFail($id);
+        $product->update($input);
+
+        return response(['Product' => $product, 'message' => 'Successful'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->json([
+            "success" => true,
+            "message" => "Product deleted successfully.",
+            "data" => $product
+        ]);
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Api\V1\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/products', [ProductController::class,'index']);
+Route::post('/products_store', [ProductController::class,'store']);
+Route::post('/products_update/{id}', [ProductController::class,'update']);
+Route::post('/products_delete/{id}', [ProductController::class,'destroy']);
