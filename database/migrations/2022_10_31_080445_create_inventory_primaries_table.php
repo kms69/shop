15486bcf281d63af4f114478c2add0ff806c0c04
kms69 <kavehmohammadi1369@gmail.com>
2022-10-31 12:03:16 +0330

    inventory primary table and its model and controller created

---
+++ database/migrations/2022_10_31_080445_create_inventory_primaries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory_primaries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('stock');
+            $table->dateTime('last_update');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory_primaries');
+    }
+};
